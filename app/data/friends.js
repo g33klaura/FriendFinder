ce245b0768f34d1f63c717cc08a08c68d5dab95d
Laura Boetcher <g33klaura@gmail.com>
2017-10-29 14:52:45 -0400

    Add initial friendsArray data

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+// Initial friends objects with array of scores
+// 'module.exports = friendsArray;' is at end of script
+
+let friendsArray = [
+  {
+    name: 'Bender Bending Rodriguez',
+    photo: 'https://uproxx.files.wordpress.com/2015/12/bender-futurama.jpg?quality=100&w=650',
+    scores: [
+      1,
+      5,
+      1,
+      2,
+      4,
+      2,
+      2,
+      1,
+      3,
+      4
+    ]
+  },
+  {
+    name: 'Dr. John A. Zoidberg',
+    photo: 'https://uproxx.files.wordpress.com/2014/10/zoidberg.jpg?quality=100&w=650',
+    scores: [
+      2,
+      1,
+      3,
+      4,
+      3,
+      5,
+      3,
+      4,
+      2,
+      2
+    ]
+  },
+  {
+    name: 'Kif Kroker',
+    photo: 'https://iparallax.files.wordpress.com/2013/12/futurama_kif_kroker.png?quality=100&width=650',
+    // Added quality and width after the fact ~ not sure if it works
+    scores: [
+      4,
+      3,
+      2,
+      3,
+      5,
+      3,
+      5,
+      3,
+      1,
+      1
+    ]
+  },
+  {
+    name: 'Morbo',
+    photo: 'https://i.pinimg.com/736x/78/66/b0/7866b00d02b4073a3a1064af4338370a--futurama-funny-shit.jpg',
+    scores: [
+      3,
+      2,
+      4,
+      5,
+      1,
+      1,
+      1,
+      2,
+      4,
+      3
+    ]
+  },
+  {
+    name: 'Lord Nibbler',
+    photo: 'http://www.behindthevoiceactors.com/_img/chars/nibbler-futurama-benders-game-18.4.jpg',
+    scores: [
+      5,
+      4,
+      5,
+      1,
+      2,
+      4,
+      4,
+      5,
+      5,
+      5
+    ]
+  }
+];
+
+
+module.exports = friendsArray;
