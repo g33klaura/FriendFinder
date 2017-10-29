3a9d630de367178da9396e8c52aad672b8276507
Laura Boetcher <g33klaura@gmail.com>
2017-10-29 14:53:07 -0400

    Add dependencies

---
+++ server.js
@@ -0,0 +1,36 @@
+// JavaScript for FuturamaFriendFinder
+/*
+Steps to complete:
+[] htmlRoutes.js
+[] apiRoutes.js
+[] Save app data in friends.js as array of objects
+[] Closest friend match is number with lease difference (so, lowest number?)
+[] Modal pop-up displaying resulting friend match (name & picture)
+[] Update readme
+*/
+
+
+// REQUIREMENTS ========================================
+// 
+
+const express = require('express');
+const bodyParse = require('body-parser');
+const path = require('path');  //right? or is this like 'fs'?  ~It is... wonder what I installed...
+
+// Will need to require friendsArray from 'friends.js' on one of these pages....
+// const friends = require(friendsArray); ***
+
+
+
+// VARIABLES ========================================
+// 
+
+
+
+// FUNCTIONS ========================================
+//
+
+
+
+// MAIN PROCESS ========================================
+// 
