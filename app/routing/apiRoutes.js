753cb0a89b9be0ce1f6ac0a90a6f6a87d7ba666d
Laura Boetcher <g33klaura@gmail.com>
2017-10-30 17:08:21 -0400

    Add GET & POST routes for api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,42 @@
 // API Routes for FriendFinder App
 
-// VARIABLES ========================================
+// REQUIREMENTS ========================================
 // 
 
+let friendsData = require('../data/friends.js');
 
 
-// FUNCTIONS ========================================
+
+// ROUTING ========================================
 //
 
+module.exports = function(app) {
+	// API GET Request
+	// Below code handles when users "visit" a page
+	// In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  // ^^^Edit explaination when working**********
+
+  // THIS NAME OK???
+  app.get('/api/friends', function(req, res) {
+  	res.json(friendsData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post('/api/friends', function(req, res) {
+  	friendsData.push(req.body);
+
+  	// Will friend matching happen here??????
+  });
+};
 
 
 // MAIN PROCESS ========================================
