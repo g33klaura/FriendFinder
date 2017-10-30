89c65c90e7550ea747579c6571ea04f7bec1dc47
Laura Boetcher <g33klaura@gmail.com>
2017-10-30 18:25:03 -0400

    Pseudo code logic for scores comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,6 @@
 let friends = require('../data/friends');
 
 
-
 // ROUTING ========================================
 //
 
@@ -15,7 +14,7 @@ module.exports = function(app) {
 	// Below code handles when users "visit" a page
 	// In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------
   // ^^^Edit explaination when working**********
 
   // THIS NAME OK???
@@ -29,12 +28,16 @@ module.exports = function(app) {
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  // -----------------------------------------------------------
 
   app.post('/api/friends', function(req, res) {
   	friends.push(req.body);
 
-  	// Will friend matching happen here??????
+  	// Will friend matching happen here?????? ~YES
+    // Needs to compare newly entered scores with exisiting friends scores
+    // Modulus doesn't do the math correctly each time...
+    // Need 5-3 & 3-5 = 2......
+
   });
 };
 
