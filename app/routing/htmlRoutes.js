18b64bde3fb7dc52a84b4eea9f29bece3c450515
Laura Boetcher <g33klaura@gmail.com>
2017-10-30 17:17:34 -0400

    Add file paths so pages load once connected to server

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,29 @@
 // HTML Routes for FriendFinder App
 
-// VARIABLES ========================================
+// DEPENDENCIES ========================================
 // 
 
+const path = require('path');  //Built-in method, not npm package
 
 
-// FUNCTIONS ========================================
+// ROUTING ========================================
 //
 
+module.exports = function(app) {
+	// HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get('/survey', function(req, res) {
+  	res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
+
+  // Defaults to home if not matching route is found
+  app.get('*', function(req, res) {
+  	res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
+};
 
 
 // MAIN PROCESS ========================================
