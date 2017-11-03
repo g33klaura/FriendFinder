eb2bf13e1cec04159786eaddc9e910f5025751f9
Laura Boetcher <g33klaura@gmail.com>
2017-11-02 23:39:29 -0400

    Fix extra brackets issue - change extended urlencoded to true

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,33 +4,33 @@
 // 
 
 let friends = require('../data/friends');
+const path = require('path');
 
 
 // ROUTING ========================================
 //
 
 module.exports = function(app) {
-	// API GET Request
-	// Below code handles when users "visit" a page
-	// In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // -----------------------------------------------------------
-  // ^^^Edit explaination once working**********
-
+	
+  // API GET Request
   app.get('/api/friends', function(req, res) {
+    // Need to get the form data to SAVE here
+
+    // app.get(path.join(__dir) )
+
   	res.json(friends);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // -----------------------------------------------------------
 
+  // API POST Requests
   app.post('/api/friends', function(req, res) {
-  	friends.push(req.body);
+  	
+    friends.push(req.body);
+
+    // save the req.body to new var
+
+
+
 
   	// Will friend matching happen here?????? ~YES
     // Needs to compare newly entered scores with exisiting friends scores
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ let app = express();
 const PORT = process.env.PORT || 8080;
 
 // Setup for Express to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
