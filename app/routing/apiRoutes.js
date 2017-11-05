4bc06833d25d4d91ac11db299bf3436fd612ab68
Laura Boetcher <g33klaura@gmail.com>
2017-11-04 20:35:10 -0400

    Got help with scores comparison; now needs to make match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@
 let friendsData = require('../data/friends.js');
 const path = require('path');
 
-let totalDifference = 0;
+let totalDifference;
 
 
 // ROUTING ========================================
@@ -27,16 +27,51 @@ module.exports = function(app) {
   // API POST Request
   app.post('/api/friends', function(req, res) {
   	
-    friendsData.push(req.body);
+    // friendsData.push(req.body);
+    var userData = req.body;
+    var userScores = userData.scores;
+    var userScoresInt = userScores.map(Number)
 
     console.log('compareFriends called from apiRoutes');  //This gets triggered
 
-    compareFriends();
+    // compareFriends();
 
-    // Will friend matching happen here?????? ~YES
-    // Needs to compare newly entered scores with exisiting friends scores
+    // Loop through friends.js except for the one that's just been added
+    for (var i = 0; i < (friendsData.length); i++) {
+    
+    var currentFriend = friendsData[i];
+    totalDifference = 0;
 
-    // Math.abs() to the rescue!
+    console.log(currentFriend.name);    
+
+    
+
+    // console.log("------------------------------------");
+    // console.log('friendsData in compareFriends()');
+    // console.log(friendsData[i]);
+    // ^^See if this gets in there... ~YES
+    // console.log(friendsData[i].name);
+    // console.log(friendsData[i].scores);
+  
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        
+        var currentFriendScore = currentFriend.scores[j];
+
+        var currentUserScores = userScoresInt[j];
+          // console.log('Current User Scores: ' + currentUserScores);
+ 
+        totalDifference += Math.abs(currentUserScores - currentFriendScore);
+
+        // console.log(totalDifference);
+
+      }
+
+      // totalDifference += Math.abs(currentUserScores - currentFriendScore);
+
+      console.log(totalDifference);
+
+  }
+  // ^^Closes overall for loop
 
   });
 };
@@ -45,32 +80,26 @@ module.exports = function(app) {
 // FUNCTIONS ========================================
 // 
 
-function compareFriends() {
 
-  // Loop through friends.js except for the one that's just been added
-  for (var i = 0; i < (friendsData.length -1); i++) {
-    
-    // console.log("------------------------------------");
-    // console.log('friendsData in compareFriends()');
-    // console.log(friendsData[i]);
-    // ^^See if this gets in there... ~YES
-    // console.log(friendsData[i].name);
-    // console.log(friendsData[i].scores);
-  }
+
+
+
 
   // Survey inputs should always be at index [5]...
   // Try comparing just index [0] to [5]
-  var friendZero = friendsData[0].scores;
-    console.log(friendZero);
+  // var friendZero = friendsData[0].scores;
+    // console.log(friendZero);
 
-  var newFriend = friendsData[5].scores;
+  // =======================
+  // USER SURVEY DATA
+  // var newFriend = friendsData[5].scores;
     // console.log(newFriend);
 
   // OMG THEY'RE INTEGERS!!!!!!! *confetti*
-  var newFriendInt = newFriend.map(Number);
-    console.log(newFriendInt);
+  // var newFriendInt = newFriend.map(Number);
+    // console.log(newFriendInt);
 
-  console.log("------------------------------------");
+  // console.log("------------------------------------");
   // Thing I found online...
   // I have NOOOOO idea what numbers it's logging tho.... :(
   // Tried !== 0, < 0, > 0, no argument
@@ -80,12 +109,37 @@ function compareFriends() {
   // var matchFriendZero = Math.abs(friendZero - newFriendInt);
     // console.log(matchFriendZero);  //NaN
 
-    friendZero.forEach(function(benderElement) {
-      console.log(benderElement);
-    })
+    //Loop over one and compare to the other
+        // for (var i = 0, i < friendZero.length;i++) {
+        // Check if we have nested arrays
+          // for (var j = 0, i < newFriend.length; i++) {
+            // if (friendZero[i] == newFriend[i] ) {
+            // recurse into the nested arrays
+            // var temp = [];
+                  // temp.push(newFriend[i]);
+                  //total matches
+              // var score = temp.length;
+              //find friend with the most total matches
 
-    newFriendInt.forEach(function(userElement) {
-      console.log(userElement);
-    })
 
-}
+            // }           
+            // else if (this[i] != array[i]) { 
+                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+                // return false;   
+    //         }
+    //     }         
+    // }
+
+
+    // friendZero.forEach(function(benderElement) {
+    //   console.log(benderElement);
+    // })
+
+    // newFriendInt.forEach(function(userElement) {
+    //   console.log(userElement);
+    // })
+
+    // var differences = benderElement - userElement;
+    // console.log(differences);
+
+// }
